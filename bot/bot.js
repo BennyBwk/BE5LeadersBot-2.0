527d214d832672c764a861efec02ae310c9ce3ec
BennyBwk <bennybwkrap@gmail.com>
2018-04-08 08:47:08 +0800

    Changed Bot to Actual Bot

--- bot/bot.js
+++ bot/bot.js
@@ -4,8 +4,8 @@ const { memorySession } = require('telegraf');
 const TelegrafFlow = require('telegraf-flow');
 const { WizardScene } = TelegrafFlow ;
 
-//const bot = new Telegraf("342044341:AAGP75aL8K3VW5YqkObmA6rXNoGSxSESkj0"); //BE5LeadersBot
-const bot = new Telegraf("395832440:AAGi128LE7h3zoJlYZKNfXg9vTqFoDLwGWA");
+const bot = new Telegraf("342044341:AAGP75aL8K3VW5YqkObmA6rXNoGSxSESkj0"); //BE5LeadersBot
+// const bot = new Telegraf("395832440:AAGi128LE7h3zoJlYZKNfXg9vTqFoDLwGWA");
 
 //---------BEGIN POLLING--------
 bot.use(memorySession());
